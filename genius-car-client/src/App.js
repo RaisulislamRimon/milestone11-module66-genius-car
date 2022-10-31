d115b690b0f6171f073b0161708b2076b53d0d08
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-31 09:27:29 +0000

    added client side setup

---
+++ genius-car-client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <button className="btn btn-warning">btn 1</button>
+      <button className="btn btn-error">btn 2</button>
+    </div>
+  );
+};
+
+export default App;
