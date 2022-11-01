4cb77f3c5b2f7e436b63d6792e07ea3f0f565ea1
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-11-01 09:09:36 +0000

    update

--- genius-car-client/src/Pages/Home/Services/Services.js
+++ genius-car-client/src/Pages/Home/Services/Services.js
@@ -3,8 +3,13 @@ import React from "react";
 const Services = () => {
   return (
     <div>
-      <div>
+      <div className="text-center my-12">
         <p className="text-2xl font-bold text-orange-600">Services</p>
+        <h1 className="text-5xl font-semibold my-5">Our Service Area</h1>
+        <p className="w-1/2 mx-auto">
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+        </p>
       </div>
     </div>
   );
