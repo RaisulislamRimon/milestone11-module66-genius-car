375b469294957f8af9f8032c7950408af27b75ff
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-11-01 10:09:42 +0000

    form

--- genius-car-client/src/Pages/Login/Login.js
+++ genius-car-client/src/Pages/Login/Login.js
@@ -10,7 +10,7 @@ const Login = () => {
           <img src={login} className="w-3/4" alt="loginImg" />
         </div>
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div className="card-body">
+          <form className="card-body">
             <h1 className="text-5xl text-center font-semibold ">Login</h1>
             <div className="form-control">
               <label className="label">
@@ -38,9 +38,9 @@ const Login = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <input type="submit" value="Login" className="btn btn-primary" />
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
