31269a00f4470c0085fa7ffdf7817f7562738cbd
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-11-01 10:06:14 +0000

    update

---
+++ genius-car-client/src/Pages/Login/Login.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import login from "../../assets/images/login/login.svg";
+
+const Login = () => {
+  return (
+    <div className="hero w-full  my-20">
+      <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+        <div className="text-center lg:text-left">
+          <img src={login} className="w-3/4" alt="loginImg" />
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <h1 className="text-5xl text-center font-semibold ">Login</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+              <label className="label">
+                <Link to="/" className="label-text-alt link link-hover">
+                  Forgot password?
+                </Link>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
